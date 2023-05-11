cce9bcc9d0acc4e5d95ad166a6a6b9be0a2131e8
AlfredHolley <92630193+AlfredHolley@users.noreply.github.com>
2023-05-11 16:20:00 +0200

    Update script.js
    intervals of confidence arround the curve

--- script.js
+++ script.js
@@ -71,8 +71,10 @@ document.addEventListener("DOMContentLoaded", function () {
         const timeline = parseFloat(timelineInput.value);
 
         // Met à jour la courbe en utilisant les nouvelles valeurs
-        plotCurve(age, height, weightBaseline, timeline);
+        plotCurve(age, height, weightBaseline, timeline, gender);
     }
+    updateCurve();
+
 });
 
 function displayResults(age, height, weightBaseline, timeline, gender, resultParagraph) {
@@ -100,8 +102,17 @@ function displayResults(age, height, weightBaseline, timeline, gender, resultPar
     resultParagraph.innerHTML = `Your basal metabolism : ${mbCaloriesConverted.toFixed(2)} Kcal.<br>Your Weight Loss : ${yNew} kg.`;
 
 }
+
+function calculateOffset(MB) {
+    var slope = (1.55 - 0.73) / (12 - 4.5);
+    var offset = 0.73 + slope * (MB - 5);
+    return offset;
+}
+
+
+
 let chart;
-function plotCurve(age, height, weight_baseline, timeline) {
+function plotCurve(age, height, weight_baseline, timeline, gender) {
     // Calculs pour générer les données du graphique
     var MB_men = 1.083 * Math.pow(weight_baseline, 0.48) * Math.pow(height/100, 0.50) * Math.pow(age, -0.13);
     var MB_women = 0.963 * Math.pow(weight_baseline, 0.48) * Math.pow(height/100, 0.50) * Math.pow(age, -0.13);
@@ -109,17 +120,181 @@ function plotCurve(age, height, weight_baseline, timeline) {
     var Timeline_trans = [];
     var y_new_m = [];
     var y_new_f = [];
+    var y_new_upper1_m = [];
+    var y_new_upper2_m = [];
+    var y_new_lower1_m = [];
+    var y_new_lower2_m = [];
+
+    var y_new_upper1_f = [];
+    var y_new_upper2_f = [];
+    var y_new_lower1_f = [];
+    var y_new_lower2_f = [];
+
+
+
 
     for (var i = 0; i <= timeline; i++) {
         Timeline.push(i);
         Timeline_trans.push(Math.sqrt(i + 1));
         y_new_m.push(i === 0 ? 0 : ( 0.3324 * MB_men + 0.6037 * Timeline_trans[i] - 0.4320 * MB_men * Timeline_trans[i]));
         y_new_f.push(i === 0 ? 0 : ( 0.3324 * MB_women + 0.6037 * Timeline_trans[i] - 0.4320 * MB_women * Timeline_trans[i]));
+       
+        let offset_m = calculateOffset(MB_men);
+        y_new_upper1_m.push(Math.min(0,y_new_m[i] + offset_m));  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+        y_new_upper2_m.push(Math.min(0, y_new_m[i] + 2*offset_m));  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+        y_new_lower1_m.push(i === 0 ? 0 : y_new_m[i] - offset_m);  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+        y_new_lower2_m.push((i === 0) ? 0 : y_new_m[i] - 2*offset_m);  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+
+        let offset_f = calculateOffset(MB_women);
+        y_new_upper1_f.push(Math.min(0,y_new_f[i] + offset_f));  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+        y_new_upper2_f.push(Math.min(0,y_new_f[i] + 2*offset_f));  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+        y_new_lower1_f.push(i === 0 ? 0 : y_new_f[i] - offset_f);  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+        y_new_lower2_f.push((i === 0) ? 0 : y_new_f[i] - 2*offset_f);  // Remplacer y_new_m par y_new_f pour la courbe des femmes
+
     }
 
     // Création du graphique avec Chart.js
     var ctx = document.getElementById('weightChart').getContext('2d');
+
     
+    var datasets = [];
+    
+    if (gender === "Men") {
+        datasets.push({
+            label: '',
+            data: y_new_m,
+            backgroundColor: 'rgba(10, 10, 100, 0.5)',  // Couleur noire
+            borderColor: 'rgba(10, 10, 100, 0.5)',  // Couleur noire
+            borderWidth: 5,  // Largeur triplée
+            pointRadius: 20,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+        });
+        datasets.push({
+            label: '70 %',
+            data: y_new_upper1_m,
+            backgroundColor: 'rgba(10, 10, 100, 0.3)',
+            borderColor: 'rgba(10, 10, 100, 0)',
+            borderWidth: 0,
+            fill: '1',
+            pointRadius: 10,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+        datasets.push({
+            label: '95 %',
+            data: y_new_upper2_m,
+            backgroundColor: 'rgba(10, 10, 200, 0.05)',
+            borderColor: 'rgba(10, 10, 200, 0)',
+            borderWidth: 0,
+            fill : "-1",
+            pointRadius: 10,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+        datasets.push({
+            label: '',
+            data: y_new_lower1_m,
+            backgroundColor: 'rgba(10, 10, 100, 0.3)',
+            borderColor: 'rgba(10, 10, 100, 0)',
+            borderWidth: 0,
+            fill: '1',
+            pointRadius: 10,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+        datasets.push({
+            label: '',
+            data: y_new_lower2_m,
+            backgroundColor: 'rgba(10, 10, 200, 0.05)',
+            borderColor: 'rgba(10, 10, 200, 0)',
+            borderWidth: 0,
+            pointRadius: 10,
+            fill : "1",
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+
+
+
+        } else if (gender === "Women") {
+        datasets.push({
+            label: '',
+            data: y_new_f,
+            backgroundColor: 'rgba(10, 100, 300, 0.5)',
+            borderColor: 'rgba(10, 100, 300, 0.5)',
+            pointRadius: 20,
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent',  // Bordure du point transparente
+
+            borderWidth: 5
+        });
+        datasets.push({
+            label: '70 %',
+            data: y_new_upper1_f,
+            backgroundColor: 'rgba(10, 100, 300, 0.3)',
+            borderColor: 'rgba(10, 100, 300, 0)',
+            borderWidth: 0,
+            fill: '1',
+            pointRadius: 10,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+        datasets.push({
+            label: '95 %',
+            data: y_new_upper2_f,
+            backgroundColor: 'rgba(10, 100, 300, 0.05)',
+            borderColor: 'rgba(10, 100, 300, 0)',
+            borderWidth: 0,
+            pointRadius: 10,
+            fill : "-1",
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+        datasets.push({
+            label: '',
+            data: y_new_lower1_f,
+            backgroundColor: 'rgba(10, 100, 300, 0.3)',
+            borderColor: 'rgba(10, 100, 300, 0)',
+            borderWidth: 0,
+            fill: '1',
+            pointRadius: 10,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+        datasets.push({
+            label: '',
+            data: y_new_lower2_f,
+            backgroundColor: 'rgba(10, 100, 300, 0.05)',
+            borderColor: 'rgba(10, 100, 300, 0)',
+            borderWidth: 0,
+            fill : "1",
+            pointRadius: 10,  // Pas de points visibles
+            pointBackgroundColor: 'transparent', 
+            pointBorderColor: 'transparent'  // Bordure du point transparente
+
+
+        });
+
+    }
+
+   
+
     if (!chart) { 
         chart = new Chart(ctx, {
         type: 'line',
@@ -127,25 +302,17 @@ function plotCurve(age, height, weight_baseline, timeline) {
             labels: Timeline,
             datasets: [
                 {
-                    label: 'Men',
-                    data: y_new_m,
-                    backgroundColor: 'rgba(0, 123, 255, 0.5)',
-                    borderColor: 'rgba(0, 123, 255, 1)',
-                    borderWidth: 1
+                    data: {
+                        labels: Timeline,
+                        datasets: datasets
+                    },
                 },
-                {
-                    label: 'Women',
-                    data: y_new_f,
-                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
-                }
             ]
         },
         options: {
             responsive: true,
             maintainAspectRatio: false,
-            aspectRatio: 1.5,
+            aspectRatio: 1,
             scales: {
                 x: {
                     title: {
@@ -162,7 +329,13 @@ function plotCurve(age, height, weight_baseline, timeline) {
             },
             plugins: {
                 legend: {
-                    position: 'bottom'
+                    position: 'bottom',
+                    labels:{
+                        filter: function(item, chart) {
+                            // Ne pas afficher le label si le dataset n'a pas de label ou si le label est vide
+                            return !!item.text;
+                        }
+                }
                 },
                 title: {
                     display: true,
@@ -173,8 +346,7 @@ function plotCurve(age, height, weight_baseline, timeline) {
     });
     } else {
         chart.data.labels = Timeline;
-        chart.data.datasets[0].data = y_new_m;
-        chart.data.datasets[1].data = y_new_f;
+        chart.data.datasets = datasets;
         chart.update(); 
     }
 }
